8b9758a52b1c3cca1273ea762483c1ef29b42647
ajay374-J <77618610+ajay374-J@users.noreply.github.com>
2025-05-23 12:50:35 +0530

    Update api.py

--- axis_bank_integration/api.py
+++ axis_bank_integration/api.py
@@ -48,100 +48,84 @@ def validate_corporate_id(Transaction_Date, Account_Number, Client_Code, Virtual
 
 @frappe.whitelist(allow_guest=True)
 def payment_process_from_bank(Transaction_Date, Client_Code, Virtual_Account_No, Reference_No, Amount, Type, Remitter_IFSC, Remitter_Name, Remitting_Bank_Branch, Remitter_Account_No):
-    doc = frappe.get_doc("Bank Integration Setting")
-
-    Acc_No = None
-    auto_submit = 0
-    for i in doc.bank_configuration:
-        company = i.company
-        cost_center = i.cost_center
-        company_ba = frappe.get_doc("Bank Account", {"name": i.bank_account})
-        auto_submit += i.auto_submit_of_payment_entry
-
-        if i.has_special_character == 1:
-                Acc_No = Virtual_Account_No[:-5] + i.special_character + Virtual_Account_No[-5:]
-
-    if Acc_No and Reference_No:
-        # corporate_code = Virtual_Account_No[0:4]
-        customer_id = Acc_No[4:]
-
-    else:
-        customer_id = Virtual_Account_No[4:]
-    
-
-    # bank_acc_no = frappe.db.get_all("Bank Account", {"party": customer_id, "bank_account_no": Remitter_Account_No})
-    # if not bank_acc_no:
-    #     msg = "Invalid Remitter Account No"
-    #     frappe.local.response['http_status_code'] = 404
-    #     return {"message": msg, "stts_flg": "N"}
-
-    # bank_acc = frappe.get_doc("Bank Account", {"party": customer_id, "bank_account_no": Remitter_Account_No})
-    cus = frappe.get_doc("Customer", customer_id)
-
-    party_type = "Customer"
-    party = cus.name
-    date = Transaction_Date
-    party_details = get_party_details(company, party_type, party, date, cost_center)
-
-    bank_account = company_ba.name
-    com_acc_details = get_bank_account_details(bank_account)
-
-    if Type == "N":
-        types = "NEFT"
-    elif Type == "R":
-        types = "RTGS"
-    elif Type == "I":
-        types = "IMPS"
-    else:
-        types = Type
-
     try:
+        # Get bank integration settings
+        doc = frappe.get_doc("Bank Integration Setting")
+        Acc_No = None
+        auto_submit = 0
+        company, cost_center, company_ba = None, None, None
+
+        for config in doc.bank_configuration:
+            company = config.company
+            cost_center = config.cost_center
+            company_ba = frappe.get_doc("Bank Account", {"name": config.bank_account})
+            auto_submit += config.auto_submit_of_payment_entry
+
+            if config.has_special_character == 1:
+                Acc_No = Virtual_Account_No[:-5] + config.special_character + Virtual_Account_No[-5:]
+
+        # Determine Customer ID
+        customer_id = (Acc_No or Virtual_Account_No)[4:]
+
+        # Validate Customer
+        if not frappe.db.exists("Customer", customer_id):
+            frappe.local.response['http_status_code'] = 404
+            return {"message": "Invalid Customer", "stts_flg": "N"}
+
         cus = frappe.get_doc("Customer", customer_id)
-        # bank_acc = frappe.get_doc("Bank Account", {"party": customer_id, "bank_account_no": Remitter_Account_No})
-        co_bank_acc = frappe.db.get_value("Bank Account", {"name": company_ba.name}, ["name"])
-        ref_no = types + "/" + Reference_No
-        # if cus and bank_acc:
-        pa=frappe.db.get_value("Payment Entry",{"reference_no":ref_no},"name")
-        if not pa:
-            if cus:
-                new_pay_ent = frappe.new_doc("Payment Entry")
-                new_pay_ent.payment_type = "Receive"
-                new_pay_ent.party_type = "Customer"
-                new_pay_ent.posting_date = Transaction_Date
-                # new_pay_ent.mode_of_payment = types
-                new_pay_ent.paid_amount = flt(Amount)
-                new_pay_ent.received_amount = flt(Amount)
-                new_pay_ent.bank_account = co_bank_acc
-                new_pay_ent.party = cus.name
-                new_pay_ent.party_name = party_details.get("party_name")
-                # new_pay_ent.party_bank_account = bank_acc.name
-                new_pay_ent.paid_from = party_details.get("party_account")
-                new_pay_ent.paid_from_account_currency = party_details.get("party_account_currency")
-                new_pay_ent.paid_to_account_currency = party_details.get("party_account_currency")
-                new_pay_ent.party_balance = party_details.get("party_balance")
-                new_pay_ent.paid_from_account_balance = party_details.get("account_balance")
-                new_pay_ent.paid_to = com_acc_details.get("account")
-                new_pay_ent.reference_no = ref_no
-                new_pay_ent.reference_date = Transaction_Date
-                new_pay_ent.source_exchange_rate = 1
-                new_pay_ent.target_exchange_rate = 1
-                
-                pay_ent = frappe.db.get_value("Payment Entry", {"reference_no": ref_no},"name")
-                if not pay_ent:
-                    if auto_submit == 1:
-                        new_pay_ent.submit()
-                        return {"message": "Payment Successful", "stts_flg": "Y"}
-                    else:
-                        new_pay_ent.save(ignore_permissions=True)
-                        frappe.db.commit()
-                        return {"message": "Payment Successful", "stts_flg": "Y"}
+        party_type = "Customer"
+        party = cus.name
 
-                else:
-                    # frappe.local.response['http_status_code'] = 409
-                    return {"message": "Duplicate Entry", "stts_flg": "N"}
+        # Get account details
+        party_details = get_party_details(company, party_type, party, Transaction_Date, cost_center)
+        com_acc_details = get_bank_account_details(company_ba.name)
 
-    except:
-        traceback = frappe.get_traceback()
-        frappe.log_error(title = 'Axis Bank Integration payment Error',message=traceback)
+        # Format payment type
+        payment_type_map = {"N": "NEFT", "R": "RTGS", "I": "IMPS"}
+        types = payment_type_map.get(Type, Type)
+        ref_no = f"{types}/{Reference_No}"
+
+        # Check for non-cancelled duplicate
+        existing_payment = frappe.db.exists("Payment Entry", {
+            "reference_no": ref_no,
+            "docstatus": ["!=", 2]
+        })
+        if existing_payment:
+            return {"message": "Duplicate Entry", "stts_flg": "N"}
+
+        # Create Payment Entry
+        payment_entry = frappe.new_doc("Payment Entry")
+        payment_entry.update({
+            "payment_type": "Receive",
+            "party_type": "Customer",
+            "posting_date": Transaction_Date,
+            "paid_amount": flt(Amount),
+            "received_amount": flt(Amount),
+            "bank_account": company_ba.name,
+            "party": cus.name,
+            "party_name": party_details.get("party_name"),
+            "paid_from": party_details.get("party_account"),
+            "paid_from_account_currency": party_details.get("party_account_currency"),
+            "paid_to_account_currency": party_details.get("party_account_currency"),
+            "party_balance": party_details.get("party_balance"),
+            "paid_from_account_balance": party_details.get("account_balance"),
+            "paid_to": com_acc_details.get("account"),
+            "reference_no": ref_no,
+            "reference_date": Transaction_Date,
+            "source_exchange_rate": 1,
+            "target_exchange_rate": 1
+        })
+
+        if auto_submit == 1:
+            payment_entry.submit()
+        else:
+            payment_entry.save(ignore_permissions=True)
+
+        return {"message": "Payment Successful", "stts_flg": "Y"}
+
+    except Exception:
+        frappe.log_error(frappe.get_traceback(), "Axis Bank Integration Payment Error")
+        frappe.local.response['http_status_code'] = 500
+        return {"message": "Payment Failed", "stts_flg": "N"}
 
 #--------------------------------------------------------------------------------------------- 
